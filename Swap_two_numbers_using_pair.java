8007db44daa47a0f3d0f7d8f0aea19a8a974c584
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-07-05 23:26:45 +0530

    Create Swap_two_numbers_using_pair.java

---
+++ Swap_two_numbers_using_pair.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static Pair < Integer, Integer > swap(Pair < Integer, Integer > swapValues) {
+        // Write your code here.
+        Pair < Integer, Integer > ans=new Pair < Integer, Integer >(swapValues.getValue(),swapValues.getKey());
+        
+
+        return ans;
+        
+
+
+    }
+}
