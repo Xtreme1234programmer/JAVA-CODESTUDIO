23279302f39f6d072376ae24c59d4e2dbdb9b58e
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-07-11 15:53:16 +0530

    Create Largest_element_in_an_array.java

---
+++ Largest_element_in_an_array.java
@@ -0,0 +1,16 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>max) max=arr[i];
+        }
+        return max;
+
+    }
+}
