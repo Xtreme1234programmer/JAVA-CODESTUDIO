ec5ecc2af8b4e54ff503c842c3162e3036504632
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-08-04 17:50:32 +0530

    Create Sort_an_arrays_of_0s_1s_2s.java

---
+++ Sort_an_arrays_of_0s_1s_2s.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static void sortArray(ArrayList<Integer> arr, int n) {
+       int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+        while (mid <= high) {
+            if (arr.get(mid) == 0) {
+                // swapping arr[low] and arr[mid]
+                int temp = arr.get(low);
+                arr.set(low, arr.get(mid));
+                arr.set(mid, temp);
+
+                low++;
+                mid++;
+
+            } else if (arr.get(mid) == 1) {
+                mid++;
+
+            } else {
+                // swapping arr[mid] and arr[high]
+                int temp = arr.get(mid);
+                arr.set(mid, arr.get(high));
+                arr.set(high, temp);
+
+                high--;
+            }
+        }
+    }
+}
