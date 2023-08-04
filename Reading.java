f367785ebc8e75ef007d8f93d3229da0ece76de0
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-08-04 17:10:45 +0530

    Create Reading.java

---
+++ Reading.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution {
+    public static String read(int n, int []arr, int target){
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                return "YES";
+            }
+
+            mpp.put(arr[i], i);
+        }
+        return "NO";
+    }
+}
