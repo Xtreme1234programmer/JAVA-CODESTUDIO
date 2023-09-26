786a18ed923e2b24687aa7c4c2185a818ad84b1b
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-09-26 17:00:44 +0530

    Create Sum_of_max_and_min.java

---
+++ Sum_of_max_and_min.java
@@ -0,0 +1,23 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+  public static int sumOfMaxMin(int[] arr, int n) {
+      // Write your code here.
+      int max=Integer.MIN_VALUE;
+      int min=Integer.MAX_VALUE;
+      for(int i=0;i<arr.length;i++)
+      {
+        if(arr[i]>max)
+        {
+          max=arr[i];
+        }
+        if(arr[i]<min)
+        {
+          min=arr[i];
+        }
+      }
+      return max+min;
+
+
+  }
+}
