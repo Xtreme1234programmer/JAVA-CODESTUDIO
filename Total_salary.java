d51a152a97dd31c463090297d51f81e9befe24c6
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-10-05 11:03:19 +0530

    Update Total_salary.java

--- Total_salary.java
+++ Total_salary.java
@@ -1,35 +1,31 @@
 import java.util.Scanner;
 
-class Solution {
-	
-	public static void main(String args[]) {
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		
-		Scanner sc = new Scanner(System.in);
-		double basic = sc.nextDouble();
-		char grade = sc.next().charAt(0);
+		int basic = s.nextInt();
+		char grade = s.next().charAt(0);
 		
 		double hra = 0.2 * basic;
-        double da = 0.5 * basic;
-		int allowance;
+		double da = 0.5 * basic;
 		
+		int allowance;
 		if(grade == 'A') {
 			allowance = 1700;
 		}
-		
 		else if(grade == 'B') {
 			allowance = 1500;
 		}
-		
 		else {
 			allowance = 1300;
 		}
 		
 		double pf = 0.11 * basic;
-		double totalSalary = basic + hra + da + allowance - pf;
 		
-		// Math.round(double d) return long
-		long ans = Math.round(totalSalary);
+		double totalSalary = basic + hra + da + allowance - pf;
+		int ans = (int) Math.round(totalSalary); //An internal function implemented in the Math class(no need to import as it is available as default) to round off the decimal values
 		System.out.println(ans);
-		   
 	}
 }
