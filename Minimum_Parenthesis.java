f0a5e0a187f7a59927e580d1d70ef9b837a2264d
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-10-24 23:57:40 +0530

    Create Minimum_Parenthesis.java

---
+++ Minimum_Parenthesis.java
@@ -0,0 +1,27 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int minimumParentheses(String pattern) {
+		// Write your code here.
+		int open=0;
+		int close=0;
+
+		for(int i=0;i<pattern.length();i++)
+		{
+			char ch=pattern.charAt(i);
+			if(ch=='('){open+=1;}
+			else{
+				if(open==0){close+=1;}
+				else{
+					open--;
+
+				}
+			}
+
+		
+
+		}
+			return open+close;
+
+	}
+}
